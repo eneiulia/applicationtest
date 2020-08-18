884f3700ec17cbd019de90945121b41f00ff67ea
iene <iene@deloittece.com>
2020-08-18 17:13:13 +0300

    .

--- src/main/java/com/company/applicationtest/service/TransactionService.java
+++ src/main/java/com/company/applicationtest/service/TransactionService.java
@@ -5,7 +5,6 @@ import com.company.applicationtest.model.Transaction;
 import com.company.applicationtest.model.User;
 import com.company.applicationtest.repository.TransactionRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -32,7 +31,6 @@ public class TransactionService {
                 flatMap(account -> account.getTransactions().stream()).collect(Collectors.toList());
     }
 
-    @Async
     private void saveTransactions(List<Transaction> transactions) {
         transactionRepository.saveAll(transactions);
     }
